bb2f8f34eba09b3d37c85110ebec972a637732c8
Ezequiel Ramos <ezequielmr94@gmail.com>
2023-09-15 22:40:59 -0300

    okay

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # nTechLab Mock
 
+## How to run?
+
+```bash
+docker run --rm -d --name ntechlab -p 5000:5000 cameritegeneric.azurecr.io/ntechlab-mock
+
+# or if you want to change the port on the host for 8956 for example
+docker run --rm -d --name ntechlab -p 8956:5000 cameritegeneric.azurecr.io/ntechlab-mock
+```
+
 ## Install
 
 ### Environment Variables
--- src/services/route_middlewares.ts
+++ src/services/route_middlewares.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { validToken } from "./token";
 
+const licenseError = false;
 
 function validAuthorization(req: Request, res: Response, next: NextFunction) {
     if (!req.headers.authorization) {
@@ -16,6 +17,19 @@ function validAuthorization(req: Request, res: Response, next: NextFunction) {
         return res.status(401).json({
             "code": "UNAUTHORIZED",
             "desc": "Invalid token."
+        });
+    }
+
+    if (licenseError) {
+        return res.status(401).json({
+            "traceback": "",
+            "code": "LICENSE_ERROR",
+            "desc": "Failed to validate FFSecurity license",
+            "original_error": {
+                "code": "LICENSE_ERROR",
+                "desc": "FKERROR_LICENSE: Invalid license",
+                "param": null
+            }
         });
     }
 
